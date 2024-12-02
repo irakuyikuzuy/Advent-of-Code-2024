81a606d79fb34e8c4ae394878f4ea6615914933c
irakuyikuzuy <1soulworker1@gmail.com>
2024-12-02 14:41:28 +0900

    day 1

---
+++ day1/day1-1.js
@@ -0,0 +1,29 @@
+const input = require("fs").readFileSync("day1/_day1-1.txt", "utf-8");
+
+const [leftList, rightList] = input
+    .trim()
+    .split("\n")
+    .map((line) => line.split(/\s+/).map(Number))
+    .reduce(
+        (acc, [left, right]) => {
+            acc[0].push(left);
+            acc[1].push(right);
+            return acc;
+        },
+        [[], []]
+    );
+
+let sum = 0;
+
+leftList.sort((a, b) => a - b);
+rightList.sort((a, b) => a - b);
+
+function zip(a, b) {
+    return a.map((k, i) => [k, b[i]]);
+}
+
+for (let [a, b] of zip(leftList, rightList)) {
+    sum += Math.abs(a - b);
+}
+
+console.log(sum);
---
+++ day1/day1-2.js
@@ -0,0 +1,20 @@
+const input = require("fs").readFileSync("day1/_day1-1.txt", "utf-8");
+
+const [leftList, rightList] = input
+    .trim()
+    .split("\n")
+    .map((line) => line.split(/\s+/).map(Number))
+    .reduce(
+        (acc, [left, right]) => {
+            acc[0].push(left);
+            acc[1].push(right);
+            return acc;
+        },
+        [[], []]
+    );
+
+const count = leftList
+    .map((a) => rightList.reduce((cnt, e) => cnt + (a === e), 0) * a)
+    .reduce((cnt, e) => cnt + e, 0);
+
+console.log(count);
